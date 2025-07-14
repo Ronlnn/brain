591c93a30f195b2d022d20a17a02fc49c73d0076
roninn <artking01kg@gmail.com>
2025-07-15 00:34:32 +0300

    vault backup: 15-07-25

--- 5 Resources/Scripts/helpers.js
+++ 5 Resources/Scripts/helpers.js
@@ -1,5 +1,7 @@
-module.exports.remove_empty_lines = async () => {
-  let content = await app.vault.read(tp.file.path(true));
-  content = content.replace(/^\s*$(?:\r\n?|\n)/gm, ""); // Удаляет пустые строки
-  await app.vault.modify(tp.file, content);
+module.exports = {
+  remove_empty_lines: async () => {
+    let content = await app.vault.read(tp.file.path(true));
+    content = content.replace(/^\s*$(?:\r\n?|\n)/gm, "");
+    await app.vault.modify(tp.file, content);
+  }
 };
